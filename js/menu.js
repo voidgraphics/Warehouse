165af12f408cfc319b5bd99429af7cc2c9f7f1fe
Adrien Leloup <nittiyh@outlook.com>
2015-07-07 19:07:22 +0200

    Whitespace

--- js/menu.js
+++ js/menu.js
@@ -2,40 +2,46 @@
 
 	"use strict";
 
+	// Getting a reference to the window.
 	var gui = require( "nw.gui" );
 	var win = gui.Window.get();
 
-	win.setShowInTaskbar = false;
-
+	// Creating the tray object
 	var tray = new gui.Tray( { icon: "img/icon.png" } );
 	tray.tooltip = "Warehouse";
+
+	// Populating the menu
 	var menu = new gui.Menu();
+
 	menu.append( new gui.MenuItem( { 
 		label: "Change server settings",
 		click: function(){
 			global.openConfig();
 		}
 	} ) );
+
 	menu.append( new gui.MenuItem( { 
 		label: "Hide window",
 		click: function(){
 			win.hide();
 		}
 	} ) );
+
 	menu.append( new gui.MenuItem( { 
 		label: "Show window",
 		click: function(){
 			win.show();
 		}
 	} ) );
+
 	menu.append( new gui.MenuItem( { 
 		label: "Quit",
 		click: function(){
 			gui.App.closeAllWindows();
 		}
 	} ) );
-	tray.menu = menu;
 
+	tray.menu = menu;
 
 	global.openConfig = function(){
 		var settings_win = gui.Window.open( 'settings.html', {
